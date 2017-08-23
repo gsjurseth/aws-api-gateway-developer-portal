3965a5918fd58c98a614f0ab5abbf19f81a8075c
Geir Sjurseth <geir.sjurseth@gmail.com>
2017-08-23 21:38:58 +0200

    wip

--- dev-portal/src/services/aws.js
+++ dev-portal/src/services/aws.js
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk'
-export const awsRegion = 'YOUR_PRIMARY_AWS_REGION'
-export const cognitoRegion = 'YOUR_COGNITO_REGION'
-export const cognitoUserPoolId = 'YOUR_COGNITO_USER_POOL_ID'
+export const awsRegion = 'eu-west-1'
+export const cognitoRegion = 'eu-west-1'
+export const cognitoUserPoolId = 'eu-west-1_ALFRdHXWT'
 export const cognitoIdentityPoolId = 'YOUR_COGNITO_IDENTITY_POOL_ID'
 export const cognitoClientId = 'YOUR_COGNITO_CLIENT_ID'
 
