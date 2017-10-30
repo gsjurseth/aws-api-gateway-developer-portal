69c5c13dc9d70b74ef1f8eaf77153b88348aad43
Geir Sjurseth <geir.sjurseth@gmail.com>
2017-10-30 10:59:49 +0100

    removed eb stuff

--- s3/ebuilder-devportal-@account@.ebuilder.io/deploymentBuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf content/*
-cd ../../dev-portal
-rm -rf build
-npm run build
-cd -
-cp -a ../../dev-portal/build/* content
-touch content/error.html
--- setup.py
+++ setup.py
@@ -3,35 +3,56 @@
 import logging
 import os
 import sys
+from golem.build_utils import build_gradle, get_arguments
+
+args = get_arguments()
 
 logging.basicConfig(level='DEBUG');
 logger = logging.getLogger('setup')
 
 env_vars = {
   "cco-dev" : {
-    "CUSTOM_DOMAIN_NAME": "apigw.ebuilder.io",
-    "LOGLEVEL": "DEBUG"
+    "eu-west-1": {
+      "CUSTOM_DOMAIN_NAME": "apigw.ebuilder.io",
+      "LOGLEVEL": "DEBUG",
+      "ENVIRONMENT": "development"
+      }
   },
 
   "cco-dev-test" : {
-    "CUSTOM_DOMAIN_NAME": "apigw-test.ebuilder.io",
-    "LOGLEVEL": "DEBUG"
+    "ap-northeast-1": {
+      "CUSTOM_DOMAIN_NAME": "api-test-ap-northeast-1.ebuilder.io",
+      "LOGLEVEL": "DEBUG",
+      "ENVIRONMENT": "test"
+    },
+    "eu-west-1": {
+      "CUSTOM_DOMAIN_NAME": "apigw-test.ebuilder.io",
+      "LOGLEVEL": "DEBUG",
+      "ENVIRONMENT": "test"
+    }
   },
+    
   "cco-prod-eu-west" : {
-    "CUSTOM_DOMAIN_NAME": "apigw-skutt.ebuilder.io",
-    "LOGLEVEL": "INFO"
+    "eu-west-1": {
+      "CUSTOM_DOMAIN_NAME": "apigw-skutt.ebuilder.io",
+      "LOGLEVEL": "INFO",
+      "ENVIRONMENT": "production"
+    }
   }
 }
 
-if ( (len(sys.argv) != 3) or not (sys.argv[1] in env_vars.keys()) ):
-  logger.error("The first argument must be an account name from this list: " + ",".join(env_vars.keys()) )
+if len(sys.argv) != 2:
+  logger.error('Required: A single argument with a base64-encoded dictionary with system information.')
   sys.exit(1)
-#endIf
 
-account = sys.argv[1]
+if args['account'] not in env_vars:
+  print 'No configuration data for the account "%s" is configured.'%args['account']
+  sys.exit(1)
 
-# not used at this point but this is how we'd grab the stage name as well
-stage = sys.argv[2]
+if args['region'] not in env_vars[args['account']]:
+  print 'No configuration data for the region "%s" is configured for this account.'%args['region']
+  sys.exit(1)
 
-for key in env_vars[account]:
-  print ':ENVIRONMENT:%s:%s:'%(key, env_vars[account][key])
+for key in env_vars[args['account']][args['region']]:
+  print ':ENVIRONMENT:%s:%s:'%(
+    key, env_vars[args['account']][args['region']][key])
